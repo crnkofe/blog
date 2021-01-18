24029209357309e320db08740906f457bf5bcd0c
Simon Mihevc <simon.mihevc@gmail.com>
2021-01-18 19:48:56 +0100

    Code style

--- 2021-01-18_context/context_demo.py
+++ 2021-01-18_context/context_demo.py
@@ -1,28 +1,42 @@
+from __future__ import annotations
+
+# with annotations a class can type constrain self
+# ie. def __iter__(self) -> LineIterator:
+# see https://www.python.org/dev/peps/pep-0563/
+
 import argparse
 import logging
-from types import *
+from io import IOBase
+from typing import List, Tuple
 
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-def create_argument_parser():
+def create_argument_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--filename", type=str, help="CSV filename")
-    parser.add_argument("-s", "--separator", type=str, help="CSV field separator character or string")
-    parser.add_argument("-r", "--header", action="store_true", help="Does CSV have a header")
+    parser.add_argument(
+        "-s", "--separator",
+        type=str, help="CSV field separator character or string")
+    parser.add_argument(
+        "-r", "--header",
+        action="store_true", help="Does CSV have a header")
     return parser
 
 
 class LineIterator(object):
 
-    def __init__(self, file_object, has_header=True, separator=";"):
+    def __init__(self,
+                 file_object: IOBase,
+                 has_header: bool = True,
+                 separator: str = ";"):
         self.file_object = file_object
         self.has_header = has_header
         self.separator = separator
         self.first_line = True
 
-    def __next__(self):
+    def __next__(self) -> List[str]:
         line = next(self.file_object)
         if self.first_line:
             next(self.file_object)
@@ -30,49 +44,56 @@ class LineIterator(object):
         if line:
             return line.strip().split(self.separator)
 
-    def __iter__(self):
+    def __iter__(self) -> LineIterator:
         return self
 
 
 class CsvFile(object):
 
-    def __init__(self, file_name: str, method: str, has_header: bool = True, separator: str = ";"):
+    def __init__(self,
+                 file_name: str,
+                 method: str,
+                 has_header: bool = True,
+                 separator: str = ";"):
         self.has_header = has_header
         self.separator = separator
-        self.header = []
         self.file_name = file_name
         self.method = method
         self.file_obj = None
 
-    def header(self) -> str:
-        return self.header
-
-    def __enter__(self):
+    def __enter__(self) -> Tuple[List[str], LineIterator]:
         if not self.file_obj:
             try:
                 self.file_obj = open(self.file_name, self.method)
             except Exception as e:
-                err = e
+                # if this is rethrown caller needs to handle the exception
+                logging.error("Failed opening file")
+                pass
 
+        header = []
         if self.has_header:
             first_line = next(self.file_obj, None)
             if first_line:
-                self.header = first_line.split(self.separator)
-        return self.header, LineIterator(self.file_obj, self.has_header, self.separator)
+                header = first_line.split(self.separator)
+        lines = LineIterator(self.file_obj, self.has_header, self.separator)
+        return header,  lines
+
     def __exit__(self, type, value, traceback):
         if value:
-            logging.error(
-                "Failed processing CSV",
-                exc_info=(type, value, traceback)
-            )
+            logging.error("Failed processing CSV",
+                          exc_info=(type, value, traceback))
         if self.file_obj:
-           self.file_obj.close()
+            self.file_obj.close()
+        # return False for leaving exception handling to user
         return True
 
 
 if __name__ == "__main__":
     argument_parser = create_argument_parser()
     args = argument_parser.parse_args()
-    with CsvFile(args.filename, "r", has_header=args.header, separator=args.separator) as (csv_header, csv_file):
+    with CsvFile(args.filename,
+                 "r",
+                 has_header=args.header,
+                 separator=args.separator) as (csv_header, csv_file):
         for line in (x for i, x in enumerate(csv_file) if i < 5):
             print(line)
