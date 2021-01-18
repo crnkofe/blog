905e5fd457f7f3e9265dee4d40093061122569a2
Simon Mihevc <simon.mihevc@gmail.com>
2021-01-18 19:16:15 +0100

    Add a sample context manager

---
+++ 2021-01-18_context/context_demo.py
@@ -0,0 +1,78 @@
+import argparse
+import logging
+from types import *
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def create_argument_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--filename", type=str, help="CSV filename")
+    parser.add_argument("-s", "--separator", type=str, help="CSV field separator character or string")
+    parser.add_argument("-r", "--header", action="store_true", help="Does CSV have a header")
+    return parser
+
+
+class LineIterator(object):
+
+    def __init__(self, file_object, has_header=True, separator=";"):
+        self.file_object = file_object
+        self.has_header = has_header
+        self.separator = separator
+        self.first_line = True
+
+    def __next__(self):
+        line = next(self.file_object)
+        if self.first_line:
+            next(self.file_object)
+            self.first_line = False
+        if line:
+            return line.strip().split(self.separator)
+
+    def __iter__(self):
+        return self
+
+
+class CsvFile(object):
+
+    def __init__(self, file_name: str, method: str, has_header: bool = True, separator: str = ";"):
+        self.has_header = has_header
+        self.separator = separator
+        self.header = []
+        self.file_name = file_name
+        self.method = method
+        self.file_obj = None
+
+    def header(self) -> str:
+        return self.header
+
+    def __enter__(self):
+        if not self.file_obj:
+            try:
+                self.file_obj = open(self.file_name, self.method)
+            except Exception as e:
+                err = e
+
+        if self.has_header:
+            first_line = next(self.file_obj, None)
+            if first_line:
+                self.header = first_line.split(self.separator)
+        return self.header, LineIterator(self.file_obj, self.has_header, self.separator)
+    def __exit__(self, type, value, traceback):
+        if value:
+            logging.error(
+                "Failed processing CSV",
+                exc_info=(type, value, traceback)
+            )
+        if self.file_obj:
+           self.file_obj.close()
+        return True
+
+
+if __name__ == "__main__":
+    argument_parser = create_argument_parser()
+    args = argument_parser.parse_args()
+    with CsvFile(args.filename, "r", has_header=args.header, separator=args.separator) as (csv_header, csv_file):
+        for line in (x for i, x in enumerate(csv_file) if i < 5):
+            print(line)
